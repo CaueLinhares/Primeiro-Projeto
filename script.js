3532523cdd709f10df9b006b32f56be18291df4b
Cauê <cauelinhares34@gmail.com>
2024-01-01 19:21:27 -0300

    add main

--- script.js
+++ script.js
@@ -11,6 +11,6 @@ function toggleMode() {
     img.setAttribute("src", "./assets/avatar.png")
   } else {
     // set tiver sem light mode, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar-ligth.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
   }
 }
